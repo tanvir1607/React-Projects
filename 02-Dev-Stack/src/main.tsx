2696716647f3a1a6e4d9f5370f5686615f265429
tanvir1607 <10viranik@gmail.com>
2026-09-14 12:38:05 +0600

    Add.tsx is removed from main.tsx

--- 02-Dev-Stack/src/main.tsx
+++ 02-Dev-Stack/src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
 import Navbar from './components/Navbar.tsx'
 import Banner from './components/Banner.tsx'
 import Technologies from './components/Technologies.tsx'
